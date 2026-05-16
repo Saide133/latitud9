a7e509ca7ddaa92d111bfe9c184634b425081e44
Lu <lucianunez995@gmail.com>
2026-05-16 01:25:00 +0000

    test commit web

--- mi-app/src/App.jsx
+++ mi-app/src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect} from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [contador, setContador] = useState(0)
+  const [nombre, setNombre] = useState("")
+
+  useEffect(() => {
+    console.log("👋 solo al aparecer")
+  }, [])
+
+  useEffect(() => {
+    console.log("🔢 contador cambió a:", contador)
+  }, [contador])
+
+  useEffect(() => {
+    console.log("📝 nombre cambió a:", nombre)
+  }, [nombre])
 
   return (
-    <>
-      <h1 style={{color:'orange'}}>holis</h1>
-    </>
+    <div>
+      <button onClick={() => setContador(contador + 1)}>
+        Contador: {contador}
+      </button>
+      <input
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+        placeholder="Escribí tu nombre"
+      />
+    </div>
   )
-  
 }
 
 export default App
