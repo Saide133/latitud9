bf9ea9cbb738ede116ce56b71a18bb22181607ce
Lucia <lucianunez995@gmail.com>
2026-05-18 17:25:56 -0300

    responsive navbar and footer

--- mi-app/src/components/NavBar/NavBar.jsx
+++ mi-app/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,20 @@
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { BsSun, BsMoon, BsList, BsX } from 'react-icons/bs'
 import { useTheme } from '../../context/ThemeContext'
-import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
-import { BsSun, BsMoon } from 'react-icons/bs'
+import './NavBar.css'
 
 const NavBar = () => {
   const { theme, toggleTheme } = useTheme()
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => setMenuOpen(!menuOpen)
+  const closeMenu = () => setMenuOpen(false)
 
   return (
     <nav className='navbar'>
-
-      <Link to='/' className='navbar__logo'>
+      <Link to='/' className='navbar__logo' onClick={closeMenu}>
         Latitud 9
       </Link>
 
@@ -25,9 +29,23 @@ const NavBar = () => {
         <CartWidget />
         <button className='navbar__theme-toggle' onClick={toggleTheme}>
           {theme === 'dark' ? <BsSun /> : <BsMoon />}
-        </button>        
+        </button>
+        <button className='navbar__hamburger' onClick={toggleMenu}>
+          {menuOpen ? <BsX /> : <BsList />}
+        </button>
       </div>
 
+      {menuOpen && (
+        <div className='navbar__mobile-menu'>
+          <ul className='navbar__mobile-links'>
+            <li><NavLink to='/category/origen' onClick={closeMenu}>Origen</NavLink></li>
+            <li><NavLink to='/category/blends' onClick={closeMenu}>Blends</NavLink></li>
+            <li><NavLink to='/category/equipamiento' onClick={closeMenu}>Equipamiento</NavLink></li>
+            <li><NavLink to='/category/kits' onClick={closeMenu}>Kits</NavLink></li>
+            <li><Link to='/cart' onClick={closeMenu}>Tu carrito</Link></li>
+          </ul>
+        </div>
+      )}
     </nav>
   )
 }
